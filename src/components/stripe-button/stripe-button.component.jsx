d3e03c2a19dcfeabed4debe8be1c74d6bcca0e2c
Standstrong <33655579+Standstrong@users.noreply.github.com>
2020-08-10 18:19:00 +0100

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_tNHjEmgTDSyYcGTSCLlninW1';
+
+
+	const onToken = token => {
+		console.log(token);
+		alert('Payment Successful');
+
+	}
+
+	return (
+		<StripeCheckout
+			label = 'pay now'
+			name = 'CROWN Clothing Ltd'
+			billigAddress
+			shippingAddress
+			image = 'https://svgshare.com/i/CUz.svg'
+			description = {`Your total is $${price}`}
+			amount = {priceForStripe}
+			panelLabel = 'Pay Now'
+			token = { onToken }
+			stripeKey = {publishableKey}
+		/>
+
+	);
+};
+
+export default StripeCheckoutButton;
+
